e5099e8ad1f6db3d93050403b9113bae85b9c161
J K <kj688895@gmail.com>
2020-09-21 04:59:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,15 @@
 class Dog
+  attr_accessor :name
+
   @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end 
