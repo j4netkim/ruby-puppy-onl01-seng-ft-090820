df72f800d7ac85df5ce5025541e42de3f3bc447b
J K <kj688895@gmail.com>
2020-09-21 05:07:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,8 +13,20 @@ class Dog
   end
 
   def self.all
+    @@all 
+  end
+
+  def self.print_all
     @@all.each do |dog|
       puts dog.name
     end 
-  end
+  end 
+
+  def save 
+    @@all << self 
+  end 
+
+  def name 
+    @name 
+  end 
 end
